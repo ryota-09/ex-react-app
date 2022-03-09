55d1f08907201a5d1132e20085fda31c8f510da4
ryota-09 <n.ryo950911@gmail.com>
2022-03-09 13:56:24 +0900

    Ex31OK

--- src/components/Ex31.tsx
+++ src/components/Ex31.tsx
@@ -1,7 +1,14 @@
 export const Ex31 = () => {
+  const pushFrag = true;
+
   return (
-    <div className="ex02">
+    <div className="ex31">
       <h2>Ex31</h2>
+      <button type="button" disabled={!pushFrag}>押せるボタン</button>
+      <button type="button" disabled={pushFrag}>押せないボタン</button>
+      <hr />
+      <input type="radio" name="radio" /><span>ラジオボタン</span>
+      <input type="radio" name="radio"/><span>ラジオボタン</span>
     </div>
   )
 }
