dc726a9b741c794236df444d4c6ca6218c477beb
ryota-09 <n.ryo950911@gmail.com>
2022-03-09 14:25:48 +0900

    EX60完了

--- src/components/Ex60.tsx
+++ src/components/Ex60.tsx
@@ -1,7 +1,20 @@
+//クリックしたイベントをひろう
+import { useState } from "react"
+
 export const Ex60 = () => {
+  const [pushFrag, setPushFrag] = useState(true);
+
+  const clickEvent = ():void => {
+    setPushFrag(!pushFrag)
+  }
+
   return (
     <div className="ex60">
-    <h2>EX60</h2>  
+    <h2>EX60</h2>
+    {/* void型のメソッドはカッコは不要 */}
+    <button type="button" onClick={clickEvent} disabled={!pushFrag}>押せるボタン</button>
+      <button type="button" onClick={clickEvent} disabled={pushFrag}>押せないボタン</button>
+      <hr />
     </div>
   )
 }
