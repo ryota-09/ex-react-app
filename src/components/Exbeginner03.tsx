3397d7f361f34647d769633c0e55cc698fed6543
ryota-09 <n.ryo950911@gmail.com>
2022-03-10 14:22:50 +0900

    初級演習3完了

--- src/components/Exbeginner03.tsx
+++ src/components/Exbeginner03.tsx
@@ -1,9 +1,36 @@
 //足し算結果を表示
 
+import { ChangeEvent, useState } from "react";
+
+import { Exbeginner03Practice } from "./subComponents/Exbeginner03Practice";
+
 export const Exbeginner03 = () => {
+  const [num1, setNum1] = useState<string>("");
+  const [num2, setNum2] = useState<string>("");
+  const [answer, setAnswer] = useState<number>(0);
+
+  const onChangeNumber1 = (event: ChangeEvent<HTMLInputElement>) => {
+    setNum1(event.target.value);
+    let newAnswer: number = Number(num1) + Number(num2);
+    setAnswer(newAnswer);
+  };
+
+  const onChangeNumber2 = (event: ChangeEvent<HTMLInputElement>) => {
+    setNum2(event.target.value);
+    let newAnswer: number = Number(num1) + Number(num2);
+    setAnswer(newAnswer);
+  };
+
   return (
     <div className="exbeginner03">
       <h2>EX-beginner03</h2>
+      <input type="text" onChange={onChangeNumber1} value={num1} />
+      <span>+</span>
+      <input type="text" onChange={onChangeNumber2} value={num2} />
+      <span>={answer}</span>
+      <p>ここはもっと良いコードがありそうです。。。</p>
+      <hr />
+      <Exbeginner03Practice />
     </div>
   );
 };
---
+++ src/components/subComponents/Exbeginner03Practice.tsx
@@ -0,0 +1,17 @@
+export const Exbeginner03Practice = () => {
+  return (
+    <div>
+      <h4>このファイルにコードを記述してください。</h4>
+    </div>
+  );
+};
+
+// クラスコンポーネントを利用する場合は下の雛形を利用する。
+// export default class Exbeginner03Practice {
+
+//   render() {
+//     return (
+
+//     )
+//   }
+// }
